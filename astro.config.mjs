16f8c446c77e125c6010f5502f3746c01f3419ae
auroramonet <mii-@tutanota.com>
2025-09-29 16:29:12 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://bunetic.github.io',
+	base: '/docs',
 	integrations: [
 		starlight({
 			title: 'My Docs',
